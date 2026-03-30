43b174bd6e38e58b815a6929eb9acc9091a8b26b
Arinjay <92075314+Jackboes@users.noreply.github.com>
2026-03-30 23:14:48 +0530

    Update WarGame.py

--- WarGame.py
+++ WarGame.py
@@ -2,8 +2,8 @@ import random
 
 suits = ("Hearts","Diamonds","Clubs","Spades")
 ranks = ("Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Jack","Queen","King","Ace")
-vaules = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
-         'Queen':10, 'King':10, 'Ace':11}
+vaules = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11,
+         'Queen':12 'King':13, 'Ace':14}
 
 class Card:
     def __init__(self,suit,rank):
@@ -13,6 +13,15 @@ class Card:
         
     def __str__(self):
         return self.rank + "of" + self.suit
+
+    def __gt__(self, other):
+        return self.value > other.value # Player 1 card > Player 2 card
+    
+    def __lt__(self, other):
+        return self.value < other.value # Player 2 card > Player 1 card
+    
+    def __eq__(self, other):
+        return self.value == other.value
     
 class Deck:
     def __init__(self):
@@ -29,17 +38,18 @@ class Deck:
         return self.deck.pop()
             
 
-class Hand:
-    def __init__(self,name):
+class Player:
+   def __init__(self,name):
         self.name = name
         self.deck = []
+        self.value = 0
         
     def remove_card(self):
         return self.deck.pop(0)
     
-    def add_card(self):
-        self.deck.append(Card)
-        self.value+= vaules[Card.rank]
+    def add_card(self,card):
+        self.deck.append(card)
+        self.value += vaules[card.rank]
     
     def __str__(self):
         return f"The Player {self.name} has {len(self.deck)} cards."
